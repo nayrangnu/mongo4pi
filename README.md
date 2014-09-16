fc9a89c38c9ea5397162d3d52f6763d149eed59e
Scott Vitale <svvitale@gmail.com>
2014-09-15 19:46:18 -0600

    Add simple install script

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ More information can be found at http://markclittle.blogspot.co.uk/2012/12/adven
 
 This binary is covered by the existing MongoDb licences, which are included in this repository as well as the
 source code repository from which it was built.
+
+## Modifications
+
+@svvitale added a simple install.sh script, following the instructions at https://emersonveenstra.net/mongodb-raspberry-pi/
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ $(id -u) != 0 ]; then
+    exec sudo -- "$0" "$@"
+    exit
+fi
+
+echo "Creating mongodb user"
+adduser --firstuid 100 --ingroup nogroup --shell /etc/false --disabled-password --gecos "" --no-create-home mongodb
+
+echo "Installing mongo to /opt/mongo..."
+tar xfz mongo.tar.gz -C /opt --owner mongodb
+
+echo "Creating runtime directories under /var"
+install -o mongodb -g nogroup -d /var/log/mongodb/
+install -o mongodb -g nogroup -d /var/lib/mongodb/
+
+echo "Installing config scripts"
+install mongod /etc/init.d
+install mongodb.conf /etc
+
+echo "Setting mongod to start on boot"
+update-rc.d mongod defaults
+
+echo "Starting mongod"
+/etc/init.d/mongod start
+
+echo "done."
+
